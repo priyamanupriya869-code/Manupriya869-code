96a9447eea86db41be41141869684f9df1b32719
Manupriya S M <priyamanupriya869@gmail.com>
2025-11-03 19:26:13 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-
 int main()
 {
-    int a, b, sum ;
-    printf ("Enter two valuees to added : ");
-    scanf ("%d%d", &a,&b);
-    sum  = a + b;
-    printf ("the sum of the two values is : %d\n", sum);
+  int i = 1;
+  while (i<=5)
+  {
+    printf ("%d\n",i);
+  ++1;
+  }
 
-
-    return 0;
 }
+return 0;
